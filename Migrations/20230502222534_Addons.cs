ec7f2c6b6009bcb52ab330f20ca59eb98ccbe73c
cvetkovskamelani <101588563+cvetkovskamelani@users.noreply.github.com>
2023-05-04 01:04:17 +0200

    Add files via upload

---
+++ Migrations/20230502222534_Addons.cs
@@ -0,0 +1,260 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Migrations
+{
+    public partial class Addons : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookGenre_Books_BooksId",
+                table: "BookGenre");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookGenre_Genre_GenreId",
+                table: "BookGenre");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Review_Books_BooksId",
+                table: "Review");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserBooks_Books_BooksId",
+                table: "UserBooks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Review_BooksId",
+                table: "Review");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserBooks",
+                table: "UserBooks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserBooks_BooksId",
+                table: "UserBooks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BookGenre",
+                table: "BookGenre");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookGenre_BooksId",
+                table: "BookGenre");
+
+            migrationBuilder.DropColumn(
+                name: "BooksId",
+                table: "Review");
+
+            migrationBuilder.DropColumn(
+                name: "BooksId",
+                table: "UserBooks");
+
+            migrationBuilder.DropColumn(
+                name: "BooksId",
+                table: "BookGenre");
+
+            migrationBuilder.RenameTable(
+                name: "UserBooks",
+                newName: "UserBookss");
+
+            migrationBuilder.RenameTable(
+                name: "BookGenre",
+                newName: "BookGenres");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BookGenre_GenreId",
+                table: "BookGenres",
+                newName: "IX_BookGenres_GenreId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserBookss",
+                table: "UserBookss",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BookGenres",
+                table: "BookGenres",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Review_BookId",
+                table: "Review",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserBookss_BookId",
+                table: "UserBookss",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookGenres_BookId",
+                table: "BookGenres",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookGenres_Books_BookId",
+                table: "BookGenres",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookGenres_Genre_GenreId",
+                table: "BookGenres",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Review_Books_BookId",
+                table: "Review",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserBookss_Books_BookId",
+                table: "UserBookss",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookGenres_Books_BookId",
+                table: "BookGenres");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookGenres_Genre_GenreId",
+                table: "BookGenres");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Review_Books_BookId",
+                table: "Review");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserBookss_Books_BookId",
+                table: "UserBookss");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Review_BookId",
+                table: "Review");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserBookss",
+                table: "UserBookss");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserBookss_BookId",
+                table: "UserBookss");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BookGenres",
+                table: "BookGenres");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookGenres_BookId",
+                table: "BookGenres");
+
+            migrationBuilder.RenameTable(
+                name: "UserBookss",
+                newName: "UserBooks");
+
+            migrationBuilder.RenameTable(
+                name: "BookGenres",
+                newName: "BookGenre");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BookGenres_GenreId",
+                table: "BookGenre",
+                newName: "IX_BookGenre_GenreId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BooksId",
+                table: "Review",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BooksId",
+                table: "UserBooks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BooksId",
+                table: "BookGenre",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserBooks",
+                table: "UserBooks",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BookGenre",
+                table: "BookGenre",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Review_BooksId",
+                table: "Review",
+                column: "BooksId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserBooks_BooksId",
+                table: "UserBooks",
+                column: "BooksId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookGenre_BooksId",
+                table: "BookGenre",
+                column: "BooksId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookGenre_Books_BooksId",
+                table: "BookGenre",
+                column: "BooksId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookGenre_Genre_GenreId",
+                table: "BookGenre",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Review_Books_BooksId",
+                table: "Review",
+                column: "BooksId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserBooks_Books_BooksId",
+                table: "UserBooks",
+                column: "BooksId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
